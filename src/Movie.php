6f283ffb19b26acf82327ed59fec3a387c0a5117
bazzzu <alex.lungu@movehut.co.uk>
2015-10-20 11:04:44 +0100

    Added functions for Object State Matcher exemple.

--- src/Movie.php
+++ src/Movie.php
@@ -2,4 +2,13 @@
 
 class Movie
 {
+//    public function isAvailableOnCinemas()
+//    {
+//        return true;
+//    }
+
+//    public function hasSoundtrack()
+//    {
+//        return true;
+//    }
 }
